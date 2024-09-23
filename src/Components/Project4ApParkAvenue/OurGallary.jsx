67f0b1096acc57181b0aebed5f470b3346162dc8
Zoya774 <zoya.rasonline@gmail.com>
2024-09-23 10:12:51 +0530

    appark avenue images

--- src/Components/Project4ApParkAvenue/OurGallary.jsx
+++ src/Components/Project4ApParkAvenue/OurGallary.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import p3img1 from '../../assets/p4img1.jpg';
 import p3img2 from '../../assets/p4img2.jpg';
 import p3img3 from '../../assets/p4img3.jpg';
-import p4img4 from '../../assets/p4img4.jpg';
-import p4img5 from '../../assets/p4img5.jpg';
-import p4img6 from '../../assets/p4img6.jpg';
-import p4img7 from '../../assets/p4img7.jpg';
-import p4img8 from '../../assets/p4img8.jpg';
+import p4img4 from '../../assets/ap1.jpg';
+import p4img5 from '../../assets/ap2.jpg';
+import p4img6 from '../../assets/ap3.jpg';
+import p4img7 from '../../assets/ap4.jpeg';
+import p4img8 from '../../assets/ap5.jpeg';
 import p4img9 from '../../assets/p4img9.jpg';
 import p4img10 from '../../assets/p4img10.jpg';
 // import candolimluxfort from "../../assets/candolimluxfort.jpg"
 
 const OurGallary = () => {
-  const images = [p3img1, p4img4 , p4img7, p4img5 , p4img6  , p4img8  ,p4img9, p4img10];
+  const images = [p3img1, p4img4 , p4img7, p4img5 , p4img6  , p4img8  ];
 
   return (
     <div className="py-10 px-6 bg-white grid grid-cols-1 md:grid-cols-2 gap-6">
